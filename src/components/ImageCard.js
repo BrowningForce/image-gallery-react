183038fd22384c08a7b9db0653e48ae1ffcdbb0c
Alexander Penkin <alpenkin3@gmail.com>
2020-05-09 12:52:31 +0300

    MVP for image gallery

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,36 +1,58 @@
-import React from 'react';
+import React from "react";
 
 const ImageCard = ({ image }) => {
-  const tags = image.tags.split(', ');
+    const tags = image.tags.split(", ");
     return (
-<div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img src={image.webformatURL} alt="Random from Pixabay" className="w-full"/>
-        <div className="px-6 py-4">
-          <div className="font-bold text-purple-500 text-xl mb2">Photo by {image.user}</div>
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <a href={image.pageURL} target="_blank" rel="noopener noreferrer">
+                <img
+                    src={image.webformatURL}
+                    alt="Random from Pixabay"
+                    className="w-full"
+                />
+            </a>
+
+            <div className="px-6 py-4">
+                <div className="font-bold text-purple-500 text-xl mb2">
+                    Photo by{" "}
+                    <a
+                        className="text-blue underline bg-gray"
+                        href={`http://pixabay.com/users/${image.user}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {image.user}
+                    </a>
+                </div>
+            </div>
+            <ul>
+                <li className="pl-6">
+                    <strong>Views: </strong>
+                    {image.views}
+                </li>
+                <li className="pl-6">
+                    <strong>Downloads: </strong>
+                    {image.downloads}
+                </li>
+                <li className="pl-6">
+                    <strong>Likes: </strong>
+                    {image.likes}
+                </li>
+            </ul>
+
+            <div className="px-6 py-4">
+                {tags.map((tag, index) => (
+                    <span
+                        key={index}
+                        className="inline-block bg-gray-200 rounded-full px-3 py-1 
+      text-sm font-semibold text-gray-700 mr-2"
+                    >
+                        #{tag}
+                    </span>
+                ))}
+            </div>
         </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            {image.views}
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            {image.downloads}
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            {image.likes}
-          </li>
-        </ul>
-         
-    <div className="px-6 py-4">
-      {tags.map((tag, index) => (<span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 
-      text-sm font-semibold text-gray-700 mr-2">
-      #{tag}
-      </span>))}
-    </div>
-    </div>
-    )
-}
+    );
+};
 
 export default ImageCard;
